156e5a64772ede1562daaad81ea8a380ed80e3cb
Khan Martin <rblxxmartin@gmail.com>
2022-02-10 14:21:13 +0600

    абоба

--- laba/f.py
+++ laba/f.py
@@ -1,22 +1,22 @@
 n = int(input())
-arr = []
-student = {}
+arr = []  #список для хранения имен студентов
+student = {} #словарь для хранения студентов
 for i in range(n):
     name, cash = input().split()
     arr.append(name)
 
-    if name in student:
-        student[name] += int(cash)
+    if name in student: #добавление студентов в словарь или суммирование денег существуюших студентов
+        student[name] += int(cash) 
     else:
         student[name] = int(cash)
 
-lucky = student[max(student, key = student.get)]
+lucky = student[max(student, key = student.get)] #переменная lucky, хранится самый богатенький буржуй
 
-fake_set = set(arr)
-bkar = list(fake_set)
-bkar.sort()
+fake_set = set(arr) #избавляюсь от повторений с списке имен
+bkar = list(fake_set) #back_to_array, возвращаю set обратно в список
+bkar.sort() #сортировка порядка имен в списке
 
-for i in range(len(student)):
+for i in range(len(student)): #вывод 
     if lucky - student[bkar[i]] != 0:
         print(bkar[i], "has to receive", lucky - student[bkar[i]], "tenge")
     if lucky == student[bkar[i]]:
