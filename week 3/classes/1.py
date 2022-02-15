88e026983a7d27f597c940f69ca987f83762a568
Khan Martin <rblxxmartin@gmail.com>
2022-02-16 01:38:55 +0600

    update

---
+++ week 3/classes/1.py
@@ -0,0 +1,12 @@
+class Person:
+
+    def getString():
+        string = input()
+        return string
+
+    def printString(string):
+        print(string.upper())
+
+p1 = Person
+p1.getString()
+p1.printString("Hello world")
